471bcf2242f5ef44046c737ba8ee74a0ebac0870
pgarrett <pgarrett@scripps.edu>
2023-01-24 16:13:27 -0800

    added spectra info

--- home.py
+++ home.py
@@ -26,6 +26,7 @@ if st.button(label='Run'):
         target_stripped_peptides = len({strip_modifications(peptide_line.sequence[2:-2]) for result in target_results for peptide_line in result.peptide_lines})
         target_charged_stripped_peptides = len({(peptide_line.charge, strip_modifications(peptide_line.sequence[2:-2])) for result in target_results for peptide_line in result.peptide_lines})
         target_peptides = len({(peptide_line.charge, peptide_line.sequence[2:-2]) for result in target_results for peptide_line in result.peptide_lines})
+        target_spectra = len({peptide_line.file_name for result in target_results for peptide_line in result.peptide_lines})
         target_protein_coverage = mean([protein_line.sequence_coverage for result in target_results for protein_line in result.protein_lines])
 
         decoy_results = [result for result in results if all(['Reverse_' in protein_line.locus_name for protein_line in result.protein_lines])]
@@ -34,6 +35,7 @@ if st.button(label='Run'):
         decoy_stripped_peptides = len({strip_modifications(peptide_line.sequence[2:-2]) for result in decoy_results for peptide_line in result.peptide_lines})
         decoy_charged_stripped_peptides = len({(peptide_line.charge, strip_modifications(peptide_line.sequence[2:-2])) for result in decoy_results for peptide_line in result.peptide_lines})
         decoy_peptides = len({(peptide_line.charge, peptide_line.sequence[2:-2]) for result in decoy_results for peptide_line in result.peptide_lines})
+        decoy_spectra = len({peptide_line.file_name for result in decoy_results for peptide_line in result.peptide_lines})
         decoy_protein_coverage = mean([protein_line.sequence_coverage for result in decoy_results for protein_line in result.protein_lines])
 
 
@@ -43,12 +45,16 @@ if st.button(label='Run'):
              'target_stripped_peptides':target_stripped_peptides,
              'target_charged_stripped_peptides':target_charged_stripped_peptides,
              'target_peptides':target_peptides,
+             'target_spectra': target_spectra,
+
              'target_protein_coverage':target_protein_coverage,
              'decoy_protein_locuses':decoy_protein_locuses,
              'decoy_protein_groups':decoy_protein_groups,
              'decoy_stripped_peptides':decoy_stripped_peptides,
              'decoy_charged_stripped_peptides':decoy_charged_stripped_peptides,
              'decoy_peptides':decoy_peptides,
+             'decoy_spectra': decoy_spectra,
+
              'decoy_protein_coverage':decoy_protein_coverage}
         data.append(d)
 
@@ -58,12 +64,12 @@ if st.button(label='Run'):
     st.download_button(label='Download Table', data=df.to_csv(), file_name='filter_compare.csv')
 
     fig = px.bar(df, x="file", y=['target_protein_locuses', 'target_protein_groups', 'target_stripped_peptides',
-                                  'target_charged_stripped_peptides', 'target_peptides'], barmode="group",
+                                  'target_charged_stripped_peptides', 'target_peptides', 'target_spectra'], barmode="group",
                  text_auto=True, title='Target Protein & Peptide Stats')
     st.plotly_chart(fig)
 
     fig = px.bar(df, x="file", y=['decoy_protein_locuses', 'decoy_protein_groups', 'decoy_stripped_peptides',
-                                  'decoy_charged_stripped_peptides', 'decoy_peptides'], barmode="group",
+                                  'decoy_charged_stripped_peptides', 'decoy_peptides', 'decoy_spectra'], barmode="group",
                  text_auto=True, title='Decoy Protein & Peptide Stats')
     st.plotly_chart(fig)
 
